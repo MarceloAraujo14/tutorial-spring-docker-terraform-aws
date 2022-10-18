e9c255f6f22418dec9259d82f619161a26343829
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-10-18 20:59:44 -0300

    first commit

---
+++ src/main/java/com/maraujo/tutorialdeploy/ProjetoModeloApplication.java
@@ -0,0 +1,13 @@
+package com.maraujo.tutorialdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoModeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maraujo/tutorialdeploy/ProjetoModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maraujo.tutorialdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
